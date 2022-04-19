9ec53235c26b181051877175cd676b338ade3d9e
Megha Dogra <76041569+Megha-prog@users.noreply.github.com>
2022-04-20 01:29:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     <main>
       <section className="menu section">
         <div className="title">
-          <h2>our menu</h2>
+          <h2>Indian Food Menu</h2>
           <div className="underline"></div>
         </div>
         <Categories categories={categories} filterItems={filterItems} />
